8121756f4f6981923f0b4f3b1febf54d3c9f8951
Tyler <tgrande@my.bcit.ca>
2024-03-21 15:08:42 -0700

    reactive functioning for navbar

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -1,8 +1,3 @@
-<script setup>
-  import { Authenticator } from "@aws-amplify/ui-vue";
-  import "@aws-amplify/ui-vue/styles.css";
-</script>
-
 <template>
   <authenticator>
     <template v-slot="{ user, signOut }">
@@ -11,3 +6,30 @@
     </template>
   </authenticator>
 </template>
+
+<script setup>
+import { onMounted, onUnmounted } from 'vue';
+import { Authenticator } from "@aws-amplify/ui-vue";
+import { Hub } from 'aws-amplify/utils';
+import { authUserStore } from '@/stores/auth';
+import { useRouter } from 'vue-router';
+
+const store = authUserStore();
+const router = useRouter();
+
+onMounted(() => {
+  const listener = (data) => {
+    if (!store.isAuthenticated) {
+      store.currentSession().then(() => {
+        if (store.isAuthenticated) {
+          router.push('/')
+        }
+      });
+    }
+  };
+  Hub.listen('auth', listener);
+  console.log('hi')
+});
+
+
+</script>
